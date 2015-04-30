5e14feda786a9f4e45ba08b0999b9bd1992b50aa
joyce <geojoyce934@gmail.com>
2015-04-30 10:00:40 +0530

    solved date problem

--- receiver.php
+++ receiver.php
@@ -1,15 +1,11 @@
 <?PHP
-
 /*$user_name = "root";
 $password = "root";
 $database = "mydb";
 $server = "localhost";*/
-
 require_once('/var/www/html/form/admin.php'); 
   $conn=mysqli_connect($server, $user_name, $password, $database);
-
 print "Connection to the Server opened";
-
 $name=$_POST['element_1'];
 $enrollment=$_POST['element_2'];
 $branch=$_POST['element_8']; 
@@ -28,17 +24,11 @@ $duration=$_POST['element_19'];
 $start=$_POST['element_16'];
 $end=$_POST['element_17'];
 $addressoforganisation=$_POST['element_7'];
-
-
 $sql = "INSERT INTO form(Name, Enrollment, Branch, Semester, Batch, Phone, Mobile, Email, Natureoftraining, Natureoforganisation, Addressoftheorganization, Fullname, Designation, Contactnumber, EmailID, Durationoftraining, StartingDate, EndDate)
 VALUES (\"$name\",\"$enrollment\",\"$branch\",$semester,\"$batch\",$phone,$mobile,\"$email\",\"$nature\",\"$natureoforganisation\",\"$fullnameoforganisation\",\"$Addressoforganisation\",\"$designation\",$contact,\"$emailid\",\"$duration\",\"$start\",\"$end\")";
-
-
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-
-
 ?>
