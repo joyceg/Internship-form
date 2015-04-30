b260560020d47b734d97972c4c36211cdd383f8f
joyce <geojoyce934@gmail.com>
2015-04-30 18:24:00 +0530

    solved contact number solving problem

--- receiver.php
+++ receiver.php
@@ -9,31 +9,31 @@ require_once('/var/www/html/form/admin.php');
   /*print "Connection to the Server opened";*/
 $sql = "CREATE TABLE IF NOT EXISTS RegistrationForm (
 
-													
+	Registration_Id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,												
 	Name varchar(50) NOT NULL,
-	Enrollment varchar(50) NOT NULL PRIMARY KEY,
+	Enrollment varchar(50) NOT NULL,
 	Branch varchar(20) NOT NULL,
 	Semester varchar(30) NOT NULL,
 	Batch varchar(35) NOT NULL,
-	Phone int(30) NOT NULL,
-	Mobile int(20) NOT NULL,
+	Phone varchar(15) NOT NULL,
+	Mobile varchar(15) NOT NULL,
 	Email varchar(30) NOT NULL,
 	Nature varchar(255) NOT NULL,
-	Natureoforganisation varchar( 400 ) NOT NULL,
-	Fullname varchar( 400 ) NOT NULL,
-	Addressoforganisation varchar( 200 ) NOT  NULL,
+	Nature_of_organisation varchar( 400 ) NOT NULL,
+	Full_name_of_the_official_addresse varchar( 400 ) NOT NULL,
+	Address_of_organisation varchar( 200 ) NOT  NULL,
 
 	Designation varchar( 50 ) NOT NULL,
-	Contact int( 20 ) NOT NULL,	
-	Emailid varchar( 50 ) NOT NULL,
+	Contact varchar( 25 ) NOT NULL,	
+	Email_id varchar( 50 ) NOT NULL,
 	Duration varchar( 20 ) NOT  NULL,
-	Start date NOT  NULL,
-	EndDate date NOT NULL
+	Start_date date NOT  NULL,
+	End_date date NOT NULL
 	
 )";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
+   /* echo "Table MyGuests created successfully";*/
 } else {
     echo "Error creating table: " . $conn->error;
 }
@@ -58,11 +58,13 @@ $start=$_POST['element_16'];
 $End=$_POST['element_17'];
 $addressoforganisation=$_POST['element_7'];
 
-$sql = "INSERT INTO RegistrationForm(Name,Enrollment,Branch,Semester, Batch, Phone, Mobile, Email, Nature, Natureoforganisation, Fullname, Addressoforganisation, Designation, Contact, Emailid, Duration, Start, EndDate)
+$sql = "INSERT INTO RegistrationForm(Name,Enrollment,Branch,Semester, Batch, Phone, Mobile, Email, Nature, Nature_of_organisation, Full_name_of_the_official_addresse, Address_of_organisation, Designation, Contact, Email_id, Duration, Start_date, End_date)
 VALUES (\"$name\",\"$enrollment\",\"$branch\",$semester,\"$batch\",$phone,$mobile,\"$email\",\"$nature\",\"$natureoforganisation\",\"$fullnameoforganisation\",\"$addressoforganisation\",\"$designation\",$contact,\"$emailid\",\"$duration\",\"$start\",\"$End\")";
+
 if (mysqli_query($conn, $sql)) {
-    echo "Thank You,Your Application has been recoreded";
+    /*echo "Thank You,Your Application has been recoreded";*/
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
+
 ?>
