9be264299390d6dc2ed0f13c67c3e82060387cef
joyce <geojoyce934@gmail.com>
2015-10-07 22:38:21 +0530

    slight modifications

--- myphp/logout.php
+++ myphp/logout.php
@@ -1,21 +1,9 @@
-<!--<html>
+<html>
 <head> </head>
 <script>
-    alert( "You have successfully logged out." );
+
     window.location = "../login.html";
+    alert( "You have successfully logged out." );
 </script>
 
-</html>
--->
-<html>
-<body>
-<?php
-    session_start();
-    header("Location:../login.html");
-	session_destroy();
-	setcookie(PHPSESSID,session_id(),time()-1);
-
-
-?>
-</body>
 </html>
