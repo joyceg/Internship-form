e5f93b3c52667f34ba190ec621788caf8fd471b1
joyce <geojoyce934@gmail.com>
2016-01-08 21:40:27 +0530

    modified letter layout

--- myphp/PrintLetter.php
+++ myphp/PrintLetter.php
@@ -12,6 +12,13 @@ if(!($_SESSION["newsession"])){
         display :  none;
     }
 }
+div.margin{
+	margin-right:2.54cm;
+    margin-left:2.54cm;
+    font-size:13;
+    font-family:Cambria;
+    text-align: justify;
+}
 </style>
 <title>Print Letter</title>
 </head>
@@ -21,6 +28,7 @@ if(!($_SESSION["newsession"])){
  </div>
   <div align="right"><input id ="printbtn" type="button" value="Print this page" onclick="window.print();" >
 </div>
+<div class="margin">
 <br>
 <br>
 <br>
@@ -29,13 +37,14 @@ if(!($_SESSION["newsession"])){
 <br>
 <br>
 <br>
-<h3 align="center">Aum Amriteswaryai Namah</h3>
+<br>
+<div align="center">Aum Amriteswaryai Namah</div>
 <?php
+
 echo "<div align=right>";
 $id=$_POST['letter_id'];
-
-$now = new DateTime();
-echo $now->format('d-m-Y');
+//$now = new DateTime();
+print date("d-m-Y <br>", time());
 echo "</div>";
 require_once('admin.php');
 $conn=mysqli_connect($server, $user_name, $password, $database);
@@ -43,7 +52,8 @@ if ($conn->connect_error) {
     header('Location: '.'failure.html');
    return false;
 }
-$sql = "SELECT * FROM RegistrationForm WHERE Registration_Id=$id";
+else
+$sql = "SELECT * FROM RegistrationForm WHERE Registration_Id=1";
 $result = $conn->query($sql);
     while($row = $result->fetch_assoc()) {
         echo "<br>".$row["Full_name_of_the_official_addresse"];
@@ -60,18 +70,18 @@ for( $i = 0; $i <= $strlen; $i++ ) {
     // $char contains the current character, so do your processing here
 }
 
-                echo "<br><h3>Subject-Request for ".$row['Nature']." for ".$row['Course'];
+                echo "<br><br><strong>Subject:-Request for ".$row['Nature']." for ".$row['Course'];
                 if($row['member2']!='')
                 echo " students";
                 else
                 echo " student";
-                echo " </h3>";
-                echo "Dear madam/sir,<br>";
+               
+                echo "</strong><br><br>Dear Sir/Madam,<br><br>";
                 if($row["School"]=='Amrita school of Engineering')
                   {
                     echo "Amrita School of Engineering (ASE) is one of the several professional " ;
                     echo"institutions under Amrita Vishwa Vidyapeetham, established under section 3";
-                    echo " of the UGC Act, 1956. Engineering (B. Tech) students studying in ASE are advised to" ;
+                    echo " of the UGC Act, 1956. Engineering (".$row["Course"].") students studying in ASE are advised to" ;
                     echo  " do Internship in reputed organizations. This will give them some practical experience";
                     echo " which will contribute substantially to their learning process.";
                   }
@@ -81,7 +91,7 @@ for( $i = 0; $i <= $strlen; $i++ ) {
                       echo" professional institutions";
                       echo " under Amrita Vishwa Vidyapeetham, established under section 3";
                       echo "of the UGC Act, 1956.";
-                      echo " Management (BBM) students studying in ASAS are advised do";
+                      echo " Management (" .$row["Course"]. ") students studying in ASAS are advised do";
                       echo "Internship in reputed organizations.";
                       echo " This will give them some practical experience which will";
                       echo " contribute substantially to their learning process.";
@@ -96,14 +106,14 @@ for( $i = 0; $i <= $strlen; $i++ ) {
                             echo $row["Name"]." wishes to do internship in your esteemed organization.";
                             echo " Your acceptance of this";
                             echo " request will encourage him greatly, and help in enhancing his";
-                            echo " academic performance.";
+                            echo " academic performance.<br><br>";
                         }
                         else
                           {
                             echo "The following students wish to do Internship in your ";
                             echo "esteemed organization. Your acceptance of this request ";
                              echo " will encourage them greatly, and help in enhancing their ";
-                              echo "academic performance.";
+                              echo "academic performance.<br><br>";
                               echo "<br>1.";
 				    if($row["Gender"]=='Male')
 				      echo 'Mr.';
@@ -143,21 +153,22 @@ for( $i = 0; $i <= $strlen; $i++ ) {
 					echo $row["member5"];
 					}
                           }
-                            echo "<br>Course:".$row["Course"]." Specialization: ".$row["Specialization"]."     Semester: ".$row["Semester"];
-                            echo "<br>".$row["Nature"]." Duraton:";
+                            echo "<br><br>Course: ".$row["Course"]." Specialization: ".$row["Specialization"]."     Semester: ".$row["Semester"];
+                            echo "<br><br>".$row["Nature"]." Duraton: ";
                             echo $row["Start_date"];
                             echo " to ";
                             echo $row["End_date"];
 
 
-                    echo"<br><br><br><br>Thanking you,";
-                    echo "<br>Ever in Amma's service,";
-                    echo "<br>Biju Kumar<br>Head-Corporate & Industry Relations";
+                    echo"<br><br>Thanking you,<br>";
+                    echo "<br>Ever in Amma's service,<br>";
+                    echo "<br><br><br><br><br><strong>Biju Kumar</strong><br>Head-Corporate & Industry Relations";
                     echo "<br>Amritapuri Campus";
 
 
         }
 
   ?>
+</div>
 </body>
 </html>
