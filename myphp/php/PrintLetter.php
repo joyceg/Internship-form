ca1e491688d14bbd4abbd69afb76e32080ad92da
joyceg <geojoyce934@gmail.com>
2016-06-09 15:10:05 +0530

    renamed the company input details

--- myphp/php/PrintLetter.php
+++ myphp/php/PrintLetter.php
@@ -146,12 +146,12 @@ else
 				      echo 'Ms. ';
 					echo $row["Name"];
 
-                              echo "<br>	2. ";
+                              echo "	2. ";
 				    if($row["Gender2"]=='Male')
 				      echo 'Mr. ';
 				      else
 				      echo 'Ms. ';
-					echo $row["member2"];
+					echo str_pad($row["member2"],  60, " ");
                               if($row["member3"]!='')
                                { echo "<br>3. ";
 				    if($row["Gender3"]=='Male')
